c7e9c2b977769583e7e71e21e2926570c037fcbc
dmisiek <dominikmisiek02@gmail.com>
2026-02-14 10:24:27 +0100

    Initial commit

---
+++ lib/src/maplibre_expressions.dart
@@ -0,0 +1,7 @@
+/// {@template maplibre_expressions}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class MaplibreExpressions {
+  /// {@macro maplibre_expressions}
+  const MaplibreExpressions();
+}
---
+++ test/src/maplibre_expressions_test.dart
@@ -0,0 +1,12 @@
+// Not required for test files
+// ignore_for_file: prefer_const_constructors
+import 'package:maplibre_expressions/maplibre_expressions.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('MaplibreExpressions', () {
+    test('can be instantiated', () {
+      expect(MaplibreExpressions(), isNotNull);
+    });
+  });
+}
